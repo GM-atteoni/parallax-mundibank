6bc90087ba7d48d5a58b0c9d229892db03607346
alyfmendonca <alyfmendonca1@gmail.com>
2019-09-06 11:10:27 -0300

    fixed bg

--- mundi.js
+++ mundi.js
@@ -1,9 +1,10 @@
 
 window.onload = function() {
-  TweenMax.to("#firstTransitionDiv", 0.3, {xPercent:-50});
-  TweenMax.from(".border-top", 0.3, {height: 0});
-  TweenMax.from(".border-bottom", 0.3, {height: 0});
-  TweenMax.from(".border-left", 0.3, {width: 0});
-  TweenMax.from(".border-right", 0.3, {width: 0});
+  TweenMax.to("#firstTransitionDiv", 0.5, {xPercent:-50, delay: 0.5});
+  TweenMax.from(".border-top", 0.5, {height: 0, delay: 0.5});
+  TweenMax.from(".border-bottom", 0.5, {height: 0, delay: 0.5});
+  TweenMax.from(".border-left", 0.5, {width: 0, delay: 0.5});
+  TweenMax.from(".border-right", 0.5, {width: 0, delay: 0.5});
+  TweenMax.from("#svgOla", 1, {opacity: 0, delay: 0.5, ease: Expo.easeIn});
 
 };
